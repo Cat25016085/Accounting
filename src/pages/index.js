5f267aa7cf87ca6562ce457000d77c53ac6c0cac
Cat25016085 <alex2501608@gmail.com>
2025-04-02 03:10:25 +0800

    index

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+// src/pages/index.js
+import { useEffect, useState } from 'react';
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch('/api/users');
+      const data = await res.json();
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
